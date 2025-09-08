870c558a0ce68fbef3d6b00530ae4ba531ca4381
Dinamiet <iceman.sm39@gmail.com>
2025-09-08 21:47:21 +0200

    Display clear resets rendered elements as well

--- src/display/clear.c
+++ src/display/clear.c
@@ -1,6 +1,9 @@
 #include "display.h"
-#include "main.h"
 #include "gpio.h"
+#include "main.h"
+#include "render.h"
+
+#include <string.h>
 
 void Display_Clear(Display disp)
 {
@@ -11,6 +14,7 @@ void Display_Clear(Display disp)
 		HAL_GPIO_WritePin(Disp1CLR_GPIO_Port, Disp1CLR_Pin, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(Disp1CLR_GPIO_Port, Disp1CLR_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(Disp1LAT_GPIO_Port, Disp1LAT_Pin, GPIO_PIN_SET);
+		memset(&display1, 0, sizeof(display1));
 		break;
 
 	case DISPLAY2:
@@ -18,6 +22,7 @@ void Display_Clear(Display disp)
 		HAL_GPIO_WritePin(Disp2CLR_GPIO_Port, Disp2CLR_Pin, GPIO_PIN_RESET);
 		HAL_GPIO_WritePin(Disp2CLR_GPIO_Port, Disp2CLR_Pin, GPIO_PIN_SET);
 		HAL_GPIO_WritePin(Disp2LAT_GPIO_Port, Disp2LAT_Pin, GPIO_PIN_SET);
+		memset(&display2, 0, sizeof(display1));
 		break;
 	}
 }
