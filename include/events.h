7e3ea1c2f01d1ec862da19ce9db2dd4b72ce2d08
Dinamiet <iceman.sm39@gmail.com>
2025-09-17 20:26:28 +0200

    Rework events to contain event data to avoid event function duplications and repeats

--- include/events.h
+++ include/events.h
@@ -1,14 +1,41 @@
 #ifndef _EVENTS_H_
 #define _EVENTS_H_
 
+#include <stdbool.h>
+
+#define MAX_BUTTONS 2
+#define MAX_KNOBS   2
+
+typedef enum _Buttons_
+{
+	BUTTON_1,
+	BUTTON_2,
+} Buttons;
+
+typedef enum _Knobs_
+{
+	KNOB_1,
+	KNOB_2,
+} Knobs;
+
 typedef enum _Events_
 {
-	EVENT_BUTTON_1_PRESS,
-	EVENT_BUTTON_2_PRESS,
-	EVENT_KNOB_1_INC,
-	EVENT_KNOB_1_DEC,
-	EVENT_KNOB_2_INC,
-	EVENT_KNOB_2_DEC,
+	EVENT_BUTTON,
+	EVENT_KNOB,
 } Events;
+
+typedef union _EventData_
+{
+	struct
+	{
+		Buttons Button;
+		bool    State;
+	};
+	struct
+	{
+		Knobs Knob;
+		bool  Increasing;
+	};
+} EventData;
 
 #endif
--- src/buzzer/buzzer.c
+++ src/buzzer/buzzer.c
@@ -1,24 +1,14 @@
 #include "buzzer.h"
 
+#include "buzz.h"
+#include "events.h"
 #include "utilities.h"
 
-#include "events.h"
-#include "buzz.h"
-
-ObserverSubscription knob1IncBuzzSub;
-ObserverSubscription knob1DecBuzzSub;
-ObserverSubscription knob2IncBuzzSub;
-ObserverSubscription knob2DecBuzzSub;
-ObserverSubscription btn1BuzzSub;
-ObserverSubscription btn2BuzzSub;
+static ObserverSubscription knobBuzzSub;
+static ObserverSubscription btnBuzzSub;
 
 void Buzzer_Setup()
 {
-	Observer_Subscribe(&eventNotifier, &knob1IncBuzzSub, EVENT_KNOB_1_INC, buzzerBuzz);
-	Observer_Subscribe(&eventNotifier, &knob1DecBuzzSub, EVENT_KNOB_1_DEC, buzzerBuzz);
-	Observer_Subscribe(&eventNotifier, &btn1BuzzSub, EVENT_BUTTON_1_PRESS, buzzerBuzz);
-
-	Observer_Subscribe(&eventNotifier, &knob2IncBuzzSub, EVENT_KNOB_2_INC, buzzerBuzz);
-	Observer_Subscribe(&eventNotifier, &knob2DecBuzzSub, EVENT_KNOB_2_DEC, buzzerBuzz);
-	Observer_Subscribe(&eventNotifier, &btn2BuzzSub, EVENT_BUTTON_2_PRESS, buzzerBuzz);
+	Observer_Subscribe(&eventNotifier, &knobBuzzSub, EVENT_KNOB, buzzerBuzz);
+	Observer_Subscribe(&eventNotifier, &btnBuzzSub, EVENT_BUTTON, buzzerBuzz);
 }
