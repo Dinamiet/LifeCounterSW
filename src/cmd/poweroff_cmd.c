b1b195aca968254822b9f4750740e4b309d81d3a
Dinamiet <iceman.sm39@gmail.com>
2025-09-24 10:06:42 +0200

    Implement poweroff command

--- src/cmd/poweroff_cmd.c
+++ src/cmd/poweroff_cmd.c
@@ -1,9 +1,15 @@
+// clang-format off
+#include "gpio.h"
+// clang-format on
 #include "cmdList.h"
 
-void poweroff_Cmd(const CLI* cli, const size_t argc, const char* const argv[]) {
-	(void)cli; // Unused
+void poweroff_Cmd(const CLI* cli, const size_t argc, const char* const argv[])
+{
 	(void)argc; // Unused
 	(void)argv; // Unused
+
+	CLI_Write(cli, "Powering off...\r\n");
+	HAL_GPIO_WritePin(PWR_Ctrl_GPIO_Port, PWR_Ctrl_Pin, GPIO_PIN_RESET);
 }
 
 const char* const poweroff_Help[] = {
