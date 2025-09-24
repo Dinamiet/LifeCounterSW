573a81ae346e5bd95718333ea753211d0207c664
Dinamiet <iceman.sm39@gmail.com>
2025-09-24 10:31:49 +0200

    Implement display command

--- src/cmd/display_cmd.c
+++ src/cmd/display_cmd.c
@@ -1,14 +1,63 @@
 #include "cmdList.h"
+#include "display.h"
+
+#include <getopt.h>
+#include <stdlib.h>
 
 void display_Cmd(const CLI* cli, const size_t argc, char* const argv[])
 {
-	(void)cli; // Unused
-	(void)argc; // Unused
-	(void)argv; // Unused
+	enum
+	{
+		cmdBrightness,
+		cmdClear,
+		cmdUnknown
+	} cmd = cmdUnknown;
+
+	uint8_t value;
+	int     c;
+	optind = 1; // Reset getopt index
+	while ((c = getopt(argc, argv, "b:c:")) != -1)
+	{
+		switch (c)
+		{
+			case 'b':
+				cmd   = cmdBrightness;
+				value = (uint8_t)strtol(optarg, NULL, 10);
+				break;
+
+			case 'c':
+				cmd   = cmdClear;
+				value = (uint8_t)strtol(optarg, NULL, 10);
+				break;
+
+			default:
+				cmd = cmdUnknown;
+				break;
+		}
+	}
+
+	switch (cmd)
+	{
+		case cmdBrightness:
+			CLI_Write(cli, "Setting brightness to %d\n", value);
+			Display_SetBrightness(value);
+			break;
+
+		case cmdClear:
+			CLI_Write(cli, "Clearing display %d\n", value);
+			Display_Clear(value);
+			break;
+
+		case cmdUnknown:
+			CLI_Write(cli, "Unknown parameters\n");
+			break;
+	}
 }
 
 const char* const display_Help[] = {
-		"Usage: display",
 		"Display settings and adjustments",
+		"Usage: display -[b:c:]",
+		" -b\tBrightness adjustment (0 - 100) ",
+		" -c\tClear display",
 		0,
 };
