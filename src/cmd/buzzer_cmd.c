62994938e09a67f9868b5160706c8235028edbb7
Dinamiet <iceman.sm39@gmail.com>
2025-10-01 22:48:06 +0200

    Remove cmdLine help strings for more flash space

--- src/cmd/buzzer_cmd.c
+++ src/cmd/buzzer_cmd.c
@@ -62,11 +62,4 @@ void buzzer_Cmd(const CLI* cli, const size_t argc, char* const argv[])
 	}
 }
 
-const char* const buzzer_Help[] = {
-		"Buzzer interactions and adjustments",
-		"Usage: buzzer -[v:f:b]",
-		"  -v {value}\tSet volume (0-100)",
-		"  -f {value}\tSelect frequency (0-100)",
-		"  -b        \tBuzz for a short duration",
-		0,
-};
+const char* const buzzer_Help[] = {0};
