8939b33006d2e54970af1ba414df4e7e75d45dbf
Dinamiet <iceman.sm39@gmail.com>
2025-10-01 22:45:35 +0200

    Loading fuel gauge configuration on powerup if required

--- include/battery.h
+++ include/battery.h
@@ -2,8 +2,13 @@
 #define _BATTERY_H_
 
 #include "bq27441.h"
+#include "events.h"
 
 #include <stdint.h>
+
+#define DEFAULT_CAPACITY          3500
+#define DEFAULT_TERMINATE_VOLTAGE 2400
+#define DEFAULT_TAPER_CURRENT     30
 
 extern BQ27441 batteryFuelGauge;
 
--- src/battery/setup.c
+++ src/battery/setup.c
@@ -19,6 +19,34 @@ void Battery_Setup()
 	batteryDevice.Handle   = &hi2c1;
 	batteryDevice.DeviceID = 0x55 << 1;
 	BQ27441_Init(&batteryFuelGauge, &batteryDevice, batteryFuelGauge_Read, batteryFuelGauge_Write);
+
+	BQ27441Flags flags;
+	BQ27441_Flags(&batteryFuelGauge, &flags);
+
+	if (flags.ResetOccured)
+	{
+		BQ27441Status status;
+		if (!BQ27441_Status(&batteryFuelGauge, &status))
+			return;
+		if (status.Sealed)
+			if (!BQ27441_Unseal(&batteryFuelGauge))
+				return;
+		if (!flags.ConfigUpdateMode)
+		{
+			if (!BQ27441_EnterMode(&batteryFuelGauge, BQ27441_MODE_CONFIGURE))
+				return;
+
+			do {
+				if (!BQ27441_Flags(&batteryFuelGauge, &flags))
+					return;
+			} while (!flags.ConfigUpdateMode);
+		}
+
+		if (!BQ27441_Configure(&batteryFuelGauge, DEFAULT_CAPACITY, DEFAULT_TERMINATE_VOLTAGE, DEFAULT_TAPER_CURRENT))
+			return;
+
+		BQ27441_SoftReset(&batteryFuelGauge);
+	}
 }
 
 static size_t batteryFuelGauge_Read(const BQ27441Device* _device, const BQ27441Address address, void* data, const size_t size)
