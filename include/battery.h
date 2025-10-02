b5d52cb4bfd3c7b7c3858da7971ae5c58c292b46
Dinamiet <iceman.sm39@gmail.com>
2025-10-02 21:57:17 +0200

    Battery parameter viewing logic and handling

--- include/battery.h
+++ include/battery.h
@@ -13,8 +13,9 @@
 extern BQ27441 batteryFuelGauge;
 
 void Battery_Setup();
-uint8_t Battery_GetSetting();
-void    Battery_SetSetting(uint8_t);
+uint8_t Battery_EnterSetting(EventSide side);
+void    Battery_ChangeSetting(EventSide side, uint8_t setting);
 void    Battery_ShowInfo(EventSide side, uint16_t value);
+void    Battery_ExitSetting(EventSide side);
 
 #endif
